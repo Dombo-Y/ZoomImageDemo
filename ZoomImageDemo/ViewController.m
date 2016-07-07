a1a7e390e9130b2edcb67e5ecaabedb3960b9178
yindongbo <1965634021@qq.com>
2016-07-07 10:20:38 +0800

    点击图片放大查看缩小后返回图片原来位置
    类似微博图片查看交互，点击图片放大查看缩小后返回图片原来位置

--- ZoomImageDemo/ViewController.m
+++ ZoomImageDemo/ViewController.m
@@ -7,21 +7,50 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "ZoomImageView.h"
+@interface ViewController () 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView * imageViewA = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"a"]];
+    imageViewA.frame = CGRectMake(100, 100, 200, 150);
+    imageViewA.userInteractionEnabled = YES;
+    imageViewA.tag = 100;
+    [self.view addSubview:imageViewA];
+    
+    UIImageView * imageViewB = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"aa"]];
+    imageViewB.frame = CGRectMake(100, 300, 200, 150);
+    imageViewB.userInteractionEnabled = YES;
+    imageViewB.tag = 200;
+    [self.view addSubview:imageViewB];
+    
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+    tap.cancelsTouchesInView = NO;
+    [imageViewA addGestureRecognizer:tap];
+    [imageViewB addGestureRecognizer:tap];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)tap:(id )sender{
+    UITapGestureRecognizer * tap = (UITapGestureRecognizer *)sender;
+    UIImageView * imageView = nil;
+    for (UIImageView * subImage in self.view.subviews)
+    {
+        if (subImage.tag == tap.view.tag)
+        {
+            imageView = subImage;
+        }
+    }
+    
+    if (imageView.image)
+    {
+        [ZoomImageView showImage:imageView];
+    }
 }
+
+
 
 @end
---
+++ ZoomImageDemo/ZoomImageView/ZoomImageView.h
@@ -0,0 +1,16 @@
+//
+//  ZoomImageView.h
+//  ZoomImageDemo
+//
+//  Created by yindongbo on 16/7/7.
+//  Copyright © 2016年 ydb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ZoomImageView : NSObject
+
++(void)showImage:(UIImageView *)avatarImageView;
+
+@end
